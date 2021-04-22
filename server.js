f5bca51cb02c0a8ac8183f3c1fc82fa198d9dfae
Jenna Garrison <78498827+jenna4285@users.noreply.github.com>
2021-04-22 15:15:00 -0500

    updated api links

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const db = require('./db.json');
+const notes = require('./db/db.json');
 
 // Dependencies
 
@@ -14,6 +14,7 @@ const PORT = 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 // get and post routes
 app.get('/', (req, res) => {
@@ -26,36 +27,36 @@ app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.
 app.get('/api/notes', (req, res) => res.json(notes));
 
 // Displays a single note, or returns false
-app.get('/api/notes/:notes', (req, res) => {
-  const chosen = req.params.notes;
+// app.get('/api/notes/:notes', (req, res) => {
+//   const chosen = req.params.notes;
 
-  console.log(notes);
+//   console.log(notes);
 
-  /* Check each note routeName and see if the same as "chosen"
-   If the statement is true, send the note back as JSON,
-   otherwise tell the user no note was found */
+//   /* Check each note routeName and see if the same as "chosen"
+//    If the statement is true, send the note back as JSON,
+//    otherwise tell the user no note was found */
 
-  for (let i = 0; i < notes.length; i++) {
-    if (chosen === notes[i].routeName) {
-      return res.json(notes[i]);
-    }
-  }
+//   for (let i = 0; i < notes.length; i++) {
+//     if (chosen === notes[i].routeName) {
+//       return res.json(notes[i]);
+//     }
+//   }
 
-  return res.json(false);
-});
+//   return res.json(false);
+// });
 
 app.post('/api/notes', (req, res) => {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
-    const newNote = req.body;
+    const newNotes = req.body;
   
     // Using a RegEx Pattern to remove spaces from newCharacter
     // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
     newNote.routeName = newNote.name.replace(/\s+/g, '').toLowerCase();
     console.log(newNotes);
   
-    characters.push(newNotes);
-    res.json(newNotes);
+    newNotes.push(notes);
+    res.json(notes);
   });
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
